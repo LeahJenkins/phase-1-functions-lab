783fece6c49901fb0ef5286c6b2d9f36ada91b0f
Leah <leah.jenkins2019@gmail.com>
2023-02-06 12:55:01 -0500

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    if (number >= 42) {
+      return number - 42
+    }
+    else {
+      return 42 - number
+    }
+}
+  console.log(distanceFromHqInBlocks(50))
+  
+  
+function distanceFromHqInFeet(number) {
+    distanceFromHqInBlocks(number)
+      if (number >= 42) {
+      return (number - 42) *264
+    }
+    else {
+      return (42 - number) *264
+    } 
+}
+console.log(distanceFromHqInFeet(50))
+
+function distanceTravelledInFeet(start, destination){
+    
+    if (destination>start){
+        return (destination - start)*264
+    }
+    else{
+        return (start-destination)*264
+    }
+}
+function calculatesFarePrice(start, destination){
+    distanceTravelledInFeet(start, destination)
+       
+    if ((start-destination || destination-start)*264 <= 400){
+            return 0
+        }
+        else if(400<((start-destination || destination-start)*264)<=2000){
+            return (((start-destination || destination-start)*264)* 0.02)-8
+        }
+        else if(2000<((start-destination || destination-start)*264)<=2500){
+            return 25
+        }
+        else{
+            return 'cannot travel that far'
+        }
+     }
+
